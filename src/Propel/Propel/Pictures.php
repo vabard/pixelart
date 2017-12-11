b6d5e1060ba0b3942e92969a68d5b2dc9c573f3e
Merel-J <julien.merel5@gmail.com>
2017-12-11 11:33:05 +0100

    local commit

---
+++ src/Propel/Propel/Pictures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Propel\Propel;
+
+use Propel\Propel\Base\Pictures as BasePictures;
+
+/**
+ * Skeleton subclass for representing a row from the 'pictures' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Pictures extends BasePictures
+{
+
+}
