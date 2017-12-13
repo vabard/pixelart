c69455721b03e470895b14f5146a4f8de1cfc0a5
Johan de La Casinière <johandelacasiniere@MacBook-Pro-de-Johan.local>
2017-12-13 10:10:10 +0100

    Local change

--- web/index_dev.php
+++ web/index_dev.php
@@ -18,5 +18,6 @@ Debug::enable();
 
 $app = require __DIR__.'/../src/app.php';
 require __DIR__.'/../config/dev.php';
+require __DIR__.'/../config/config-propel.php';
 require __DIR__.'/../src/controllers.php';
 $app->run();
