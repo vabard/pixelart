576f16c23ba766ee14ddf866cbd0c40187d49c6b
RwanR <erwann.bocher@gmail.com>
2017-12-19 12:38:22 +0100

    Admin pictures list

--- src/controllers_admin.php
+++ src/controllers_admin.php
@@ -3,6 +3,8 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Propel\Propel\Base\UsersQuery;
+use Propel\Propel\Base\PicturesQuery;
+use Propel\Propel\Base\CategoriesQuery;
 
 $app->get('/loginadmin', function(Request $request) use ($app) {
     return $app['twig']->render('admin/login_admin.html.twig', array(
@@ -60,6 +62,62 @@ $adminGroup->match('/useredit/{id}', function ($id, Request $request) use ($app)
 })
 ->method('GET|POST')
 ->bind('admin_useredit')
+;
+$adminGroup->get('pictureslist', function () use ($app) {
+    
+    $pictures = Propel\Propel\PicturesQuery::create()
+            ->joinWithUsers()
+            ->joinWithCategories()
+            //->filterByState('2')
+            ->orderByState()
+            ->orderByDateInsert('desc')
+            //->paginate($page=$p, $maxPerPage=15);
+            ->find();
+    
+    $categories = Propel\Propel\CategoriesQuery::create()
+            ->orderByTitle('asc')
+            ->find();
+    
+    // on transmet à notre template les données (toujours un array!)
+    return $app['twig']->render('admin/pictureslist.html.twig', [
+        'pictures' => $pictures,
+//        'paginate' => [
+//            'results'  => $pictures->getNbResults(),
+//            'firstpage' => $pictures->getFirstPage(),
+//            'lastpage' => $pictures->getLastPage(),
+//            'currentpage' => $pictures->getPage(),
+//            'islastpage' => $pictures->isLastPage(), //return boolean true (1) if the current page is the last page
+//            'firstindex' => $pictures->getFirstIndex(),
+//            'lastindex' => $pictures->getLastIndex(), 
+//            'getNextPage' => $pictures->getNextPage()   
+//        ],
+        'categories' => $categories
+    ]);
+})
+->bind('admin_pictureslist')
+;
+$adminGroup->get('validate/{id}', function ($id) use ($app) {
+    PicturesQuery::create()
+            ->filterByIdPictures($id)
+            ->update(array('State' => '2'));
+    return $app->redirect($app['url_generator']->generate('admin_pictureslist'));
+    
+})
+->bind('admin_validate')
+;
+$adminGroup->get('deletepicture/{id}', function ($id) use ($app) {
+    PicturesQuery::create()->filterByIdPictures($id)->delete();
+    return $app->redirect($app['url_generator']->generate('admin_pictureslist'));
+})
+->bind('admin_deletepicture')
+;
+$adminGroup->get('categorieslist', function() use ($app){
+    $categories = CategoriesQuery::create()->find();
+    return $app['twig']->render('admin/categorieslist.html.twig', [
+        'categories' => $categories
+    ]);
+})
+->bind('admin_categorieslist')
 ;
 
 $app->mount('/admin', $adminGroup);
