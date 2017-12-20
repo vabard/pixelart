9732127303ea8d6577989a10879b55fa1e393e18
Merel-J <julien.merel5@gmail.com>
2017-12-20 10:43:45 +0100

    angement de titre

--- propel.php
+++ propel.php
@@ -4,18 +4,18 @@ return [
     'propel' => [
         'paths' => [
             // The directory where Propel expects to find your `schema.xml` file.
-            'schemaDir' => '/Applications/mamp/htdocs/pixelart',
+            'schemaDir' => 'xampp/htdocs/pixelart',
 
             // The directory where Propel should output generated object model classes.
-            'phpDir' => '/Applications/mamp/htdocs/pixelart/src',
+            'phpDir' => 'xampp/htdocs/pixelart/src',
         ],
         'database' => [
             'connections' => [
                 'default' => [
                     'adapter' => 'mysql',
-                    'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=pixelart',
+                    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=pixelart',
                     'user' => 'root',
-                    'password' => 'root',
+                    'password' => '',
                     'settings' => [
                         'charset' => 'utf8'
                     ]
